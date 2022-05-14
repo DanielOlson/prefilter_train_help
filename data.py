56b49783a0201e280b40cf9aa98a0a141b41d02b
danielolson <dulcicannon@gmail.com>
2022-05-14 08:44:02 -0600

    Fixed typo with ambiguous aminos

--- data.py
+++ data.py
@@ -213,15 +213,15 @@ amino_frequencies = torch.tensor([0.074,
                                   0.013,
                                   0.033,
                                   0.068])
-amino_n_to_v = torch.zeros(len(amino_a_to_n), 20)
+amino_n_to_v = torch.zeros(len(amino_n_to_a), 20)
 for i in range(20):
     amino_n_to_v[i,i] = 1.0
 
 amino_n_to_v[amino_a_to_n['B'],amino_a_to_n['D']] = 0.5
 amino_n_to_v[amino_a_to_n['B'],amino_a_to_n['N']] = 0.5
 
-amino_n_to_v[amino_a_to_n['Z'],amino_a_to_n['I']] = 0.5
-amino_n_to_v[amino_a_to_n['Z'],amino_a_to_n['J']] = 0.5
+amino_n_to_v[amino_a_to_n['Z'],amino_a_to_n['Q']] = 0.5
+amino_n_to_v[amino_a_to_n['Z'],amino_a_to_n['E']] = 0.5
 
 amino_n_to_v[amino_a_to_n['J'],amino_a_to_n['I']] = 0.5
 amino_n_to_v[amino_a_to_n['J'],amino_a_to_n['L']] = 0.5
